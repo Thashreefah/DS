47ed873e4d23cc017eabfe95c3fe446f52833b49
Thashreefah <97940475+Thashreefah@users.noreply.github.com>
2022-02-22 12:21:42 +0530

    Add files via upload

---
+++ linkedlist6.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+	int data;
+	struct Node *left,*right;
+	Node(int data)
+	{
+		this->data=data;
+		left=right=NULL;
+	}
+};
+bool isBSTUtil(struct Node *root,Node *&prev)
+{
+	if(root)
+	{
+		if(!isBSTUtil(root->left,prev))
+		return false;
+		if(prev!=NULL&&root->data<=prev->data)
+		return false;
+		prev=root;
+		return isBSTUtil(root->right,prev);
+	}
+	return true;
+}
+bool isBST(Node *root)
+{
+	Node *prev=NULL;
+	return isBSTUtil(root,prev);	
+}
+int main()
+{
+//	struct Node *root=new Node(200);
+//	root->left=new Node(6);
+//	root->left->right=new Node(80);
+//	root->left->right->left=new Node(9);
+//	root->left->right->right=new Node(100);
+//	root->left->right->right->right=new Node(150);
+//	root->left->right->left->left=new Node(7);
+//	root->left->right->left->right=new Node(30);
+//	root->left->right->left->right->left=new Node(17);
+//	root->left->right->left->right->right=new Node(65);
+//	root->left->right->left->right->right->left=new Node(58);
+    struct Node *root = new Node(7);
+    root->left = new Node(5);
+    root->right = new Node(8);
+    root->left->left = new Node(3);
+    root->left->right = new Node(6);
+
+	if(isBST(root))
+	cout<<"Is BST";
+	return 0;
+}
+	
